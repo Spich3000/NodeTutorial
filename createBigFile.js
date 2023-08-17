b1b82c42785858cdf91e3d79bd925b45d08dfb6b
Spich <spichakovda@gmail.com>
2023-08-18 01:47:19 +0300

    end of node tutorial

---
+++ createBigFile.js
@@ -0,0 +1,6 @@
+// create big file 
+
+const {writeFile, writeFileSync} = require("fs")
+for (let i = 0; i < 10000; i++) {
+    writeFileSync("./content/big.txt", `Hello world ${i}\n`, {flag: "a"})
+}
